926f85a22fc1cb815147233a51dfbac62024ea13
Crowdbotics <team@crowdbotics.com>
2020-07-15 09:00:45 +0000

    Update components

--- src/features/BlankScreen28933/screens/blank.js
+++ src/features/BlankScreen28933/screens/blank.js
@@ -343,38 +343,6 @@ export class _Blank extends React.Component {
                     backgroundSize: "auto"
                   }}
                 >
-                  <Button
-                    style={{
-                      width: 100,
-                      marginLeft: 0,
-                      marginRight: 0,
-                      marginTop: 5,
-                      marginBottom: 5,
-                      paddingLeft: 5,
-                      paddingRight: 5,
-                      paddingTop: 5,
-                      paddingBottom: 5,
-                      overflow: "visible",
-                      textAlign: "center",
-                      verticalAlign: "baseline",
-                      fontSize: 20,
-                      color: "#000000",
-                      backgroundColor: "#3366FF",
-                      fontStyle: "normal",
-                      fontWeight: "normal",
-                      borderColor: "#000000",
-                      borderStyle: "solid",
-                      borderWidth: 0,
-                      borderLeftWidth: 0,
-                      borderRightWidth: 0,
-                      borderTopWidth: 0,
-                      borderBottomWidth: 0,
-                      borderRadius: 0
-                    }}
-                    onPress={() => alert("Pressed!")}
-                  >
-                    Press me!
-                  </Button>
                   <Slider
                     value={50}
                     minimumValue={0}
@@ -505,36 +473,6 @@ export class _Blank extends React.Component {
                     backgroundSize: "auto"
                   }}
                 >
-                  <Text
-                    style={{
-                      marginLeft: 0,
-                      marginRight: 0,
-                      marginTop: 5,
-                      marginBottom: 5,
-                      paddingLeft: 5,
-                      paddingRight: 5,
-                      paddingTop: 5,
-                      paddingBottom: 5,
-                      overflow: "visible",
-                      textAlign: "left",
-                      verticalAlign: "baseline",
-                      fontSize: 20,
-                      color: "#000000",
-                      backgroundColor: "#ffffff",
-                      fontStyle: "normal",
-                      fontWeight: "normal",
-                      borderColor: "#000000",
-                      borderStyle: "solid",
-                      borderWidth: 0,
-                      borderLeftWidth: 0,
-                      borderRightWidth: 0,
-                      borderTopWidth: 0,
-                      borderBottomWidth: 0,
-                      borderRadius: 0
-                    }}
-                  >
-                    Sample text content
-                  </Text>
                   <Datepicker
                     style={{
                       width: "100%",
@@ -648,6 +586,68 @@ export class _Blank extends React.Component {
           <Button
             style={{
               width: "100%",
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 5,
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              textAlign: "center",
+              verticalAlign: "baseline",
+              fontSize: 20,
+              color: "#000000",
+              backgroundColor: "#3366FF",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0
+            }}
+            onPress={() => alert("Pressed!")}
+          >
+            Press me!
+          </Button>
+          <Text
+            style={{
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 5,
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              fontSize: 20,
+              color: "#000000",
+              backgroundColor: "#ffffff",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0
+            }}
+          >
+            Sample text content
+          </Text>
+          <Button
+            style={{
+              width: 100,
               marginLeft: 0,
               marginRight: 0,
               marginTop: 5,
